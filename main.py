410768cdb2a5c4bfb17d44a71c2d03eeb074ad20
Kratos-Krypto <65350603+Kratos-Krypto@users.noreply.github.com>
2020-12-11 03:04:21 +0000

    Initalising and plan for classes

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
-menu = True
-while menu:
-  choice = int(input("1. Edit mode\n2. Analysis mode\n3. Exit\n"))
-  if choice == 1:
-    edit = True
+class Expense:
+  def __init__(self, expDesc, expAmount, expCategory):
+    self.expDesc = expDesc
+    self.expAmount = expAmount
+    self.expCategory = expCategory
 
-    while edit == True:
-      print("edit")
-      edit = False
+class Expenses:
+  def __init__(self):
+        self.Expenses = []
 
-  if choice == 2:
-    analysis = True
-  
-    while analysis == True:
-      print("analysis")
-      analysis = False
+  def byCat():
+    #A breakdown of the users spending by category
+ 
+  def getAvg():
+    #Display average spend per item
 
-  if choice == 3:
-    menu = False
-  
+  def total():
+    #Display total expenditure
+   
+#------------------------------------------------------------------------- Menu
+def main():
+  menu = True
+
+  while menu:
+    choice = int(input("1. Edit mode\n2. Analysis mode\n3. Exit\n"))
+   
+    if choice == 1:
+
+      edit = True
+
+      while edit == True:
+
+        print("edit")
+        edit = False
+
+    if choice == 2:
+
+      analysis = True
+   
+      while analysis == True:
+
+        print("analysis")
+        analysis = False
+
+    if choice == 3:
+
+      menu = False
+
