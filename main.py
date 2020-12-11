1c1c6ade371cb816fdbc1bb77936bea427251384
peterjfgill <65350897+peterjfgill@users.noreply.github.com>
2020-12-11 04:39:50 +0000

    Update main.py with linked list
    Updated main.py with partially completed linked list functionality, structure and methods also added for readability.

--- main.py
+++ main.py
@@ -3,46 +3,68 @@ class Expense:
     self.expDesc = expDesc
     self.expAmount = expAmount
     self.expCategory = expCategory
+    self.next = None
 
-class Expenses:
+  def set_next(self, newNext):
+        self.next = newNext
+
+class ExpenseList:
   def __init__(self):
-        self.Expenses = []
-
+      self.head = None
   def byCat():
     #A breakdown of the users spending by category
- 
+    pass
   def getAvg():
     #Display average spend per item
-
+    pass
   def total():
     #Display total expenditure
-   
-#------------------------------------------------------------------------- Menu
+    pass
+
+  def addexpense(self, desc, amount, cat):
+    newExpense = Expense(desc, amount, cat)
+    newExpense.set_next(self.head)
+    self.head = new_node
+
 def main():
   menu = True
 
   while menu:
-    choice = int(input("1. Edit mode\n2. Analysis mode\n3. Exit\n"))
-   
+    choice = int(input("Expense System\n1. Edit mode\n2. Analysis mode\n3. Exit\n"))
+    
     if choice == 1:
 
       edit = True
 
       while edit == True:
+        
+        choice = int(input("Edit Mode\n1. Add expense\n2. Remove expense\n3. Exit\n"))
+        
+        if choice == 1:
+          #Add expense to linked list
+          desc = input("Description of expense: ")
+          amount = float(input("Enter the cost of this expense: "))
+          cat = input("Enter the category this expense falls under: ")
+          ExpenseList.addexpense()
 
-        print("edit")
-        edit = False
+        if choice == 2:
+        
 
-    if choice == 2:
+        if choice == 3:
+          #Exit to main menu
+          edit = False
+
+    elif choice == 2:
 
       analysis = True
-   
+    
       while analysis == True:
 
         print("analysis")
         analysis = False
 
-    if choice == 3:
+    elif choice == 3:
 
       menu = False
 
+main()
